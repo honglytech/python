9c1ecad86858036c03eee6ad736daae1e9326c27
lyhd <lyhong.rupp@gmail.com>
2020-11-28 15:30:33 +1100

    python_data_types

---
+++ PythonTutorial/app.py
@@ -0,0 +1,18 @@
+person_name = "Tom"
+
+print(type(person_name))
+print("Hello " + person_name + "!")
+print(person_name + " likes coding")
+
+person_name = "John"
+print(type(person_name))
+print(person_name + " does not like playing games")
+print(person_name + " is teaching Python programming")
+
+person_height = 1.8
+print(type(person_height))
+print(person_height)
+
+is_beginner = True
+print(type(is_beginner))
+print(is_beginner)
