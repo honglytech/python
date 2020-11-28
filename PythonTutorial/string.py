08d27e2514467691be0a87bdb0431f38ff041e46
lyhd <lyhong.rupp@gmail.com>
2020-11-28 15:33:30 +1100

    python_string_functions

---
+++ PythonTutorial/string.py
@@ -0,0 +1,19 @@
+print("Working with \nPython String")
+
+print("Working with Python \"String\"")
+
+my_string = "I Want My Text To Be Changed"
+print(my_string.lower())
+print(my_string.upper())
+
+my_string = "Python is the most popular programming language"
+print(my_string.upper().isupper())
+print(my_string.lower().islower())
+
+my_string = "Python is the most popular programming language"
+print(len(my_string))
+print(my_string[0])
+print(my_string.index("o"))
+
+my_string = "Python"
+print(my_string.replace("Py", "Hi"))
