676a7c98e2dd74a5c08fedf443ae6319c952a847
lyhd <lyhong.rupp@gmail.com>
2020-12-05 16:25:26 +1100

    number in python

---
+++ PythonTutorial/number.py
@@ -0,0 +1,40 @@
+# print(60)
+#
+# print(100.12345)
+#
+# print(-200.1234)
+#
+# print(100 * (2 + 100))
+#
+# print(10 % 3)
+#
+# my_num = 10
+# print("Number " + str(my_num))
+#
+# my_num = -12341231.13
+# print(abs(my_num))
+#
+# print(pow(2, 5))
+# # 2 x 2 x 2 x 2 x 2
+#
+# print(max(100, 200, 999, 1000))
+# print(min(100, 200, 800, 700, 0))
+#
+# print(round(123.6))
+#
+
+from math import *
+
+
+print(type(ceil(30.5)))
+
+print(sqrt(1024))
+print(type(sqrt(20)))
+
+
+
+import math
+
+print(math.floor(10.123))
+
+print(math.pi)
