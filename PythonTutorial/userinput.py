8f62e6ac6259c1fe43bf3dfa55b54b8d2d249e2e
lyhd <lyhong.rupp@gmail.com>
2020-12-12 16:12:32 +1100

    got user input

---
+++ PythonTutorial/userinput.py
@@ -0,0 +1,10 @@
+# my_laptop = input("Enter your laptop brand: ")
+# print("My laptop is " + my_laptop)
+# print("Nice!!")
+# laptop_price = input("My I know the price as well? How much is it? ")
+# print("Well, it is around $" + laptop_price)
+
+num_one = input("Enter first number: ")
+num_two = input("Enter second number: ")
+result = float(num_one) + float(num_two)
+print("The result of this addition: " + str(result))
